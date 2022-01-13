3929c868850d2225a50364c85703a774c36d00ad
sosbullying <94664127+sosbullying@users.noreply.github.com>
2022-01-12 21:58:49 -0300

    Add files via upload

---
+++ assets/zendesk-embeddable.js
@@ -0,0 +1,1 @@
+var script = document.createElement('script');script.type = 'text/javascript';script.id = 'ze-snippet';script.src = 'https://static.zdassets.com/ekr/snippet.js?key=8588475c-518c-4ac8-92b1-ada987f5ff8a';document.getElementsByTagName('head')[0].appendChild(script);
